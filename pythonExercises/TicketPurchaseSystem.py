887e10a141270a54d878d16386d351adb88867ed
MeriemLina <merliah1706@gmail.com>
2024-12-09 10:14:33 +0100

    second exercise

--- pythonExercises/TicketPurchaseSystem.py
+++ pythonExercises/TicketPurchaseSystem.py
@@ -8,6 +8,6 @@ def calculate_price():
     nbr_tickets = int(input('how many tickets would you like to buy?'))
     price = nbr_tickets * 15.50
     print(f"the total cost is : {price}")
-    print('enjoy the show!')
+    print('enjoy teh show!')
 
 ticket_checking(input('enter your name'))
